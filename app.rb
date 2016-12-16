2303e41b0e8c70e55dfd8d8b8da06bee8aa32899
Vic Lam <lam.vic.k@gmail.com>
2016-12-16 13:56:07 -0500

    Who has a favorite song!?

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    'My name is Vic'
+  end
+
+  get '/hometown' do
+    'My hometown is Brooklyn, NY'
+  end
+
+  get '/favorite-song' do
+    'My favorite song is not definable'
+  end
+
 end
